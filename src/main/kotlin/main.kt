129f84e6fd6b40a1827243db078f811c0be9f440
Dennis Kostyuk <dk0110@yandex.ru>
2025-02-28 00:18:57 +0300

    Correct scrypt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,5 @@
 fun main() {
     val likes: Int = 142
-    val result = if (likes.toString().last()=='1') "человеку" else "людям"
+    val result = if (likes.toString().last()=='1' && likes.toString().takeLast(2)!="11") "человеку" else "людям"
     println("Понравилось $likes $result")
 }
